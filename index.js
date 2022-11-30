330cd317184e8e52366e094a0cfae0dc58a56f22
notgrowthnotvalue <96894573+notgrowthnotvalue@users.noreply.github.com>
2022-11-30 14:46:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-import { menuArray } from "./data.js"
+import { menuArray } from "./data.js";
 
 let orderArray = [];
 let total = 0;
 
-
 const foodItem = document.addEventListener("click", function (event) {
-    if (event.target.dataset.plus) {
-        addToOrder(event.target.dataset.plus)
-    }
-})
+  if (event.target.dataset.plus) {
+    addToOrder(event.target.dataset.plus);
+  }
+});
 
 function addToOrder(item) {
-    for (let member of menuArray) {
-        if (member.name === item) {
-            orderArray.push([member.name, member.price]);
-            total += member.price;
-        }
+  for (let member of menuArray) {
+    if (member.name === item) {
+      orderArray.push([member.name, member.price]);
+      total += member.price;
     }
-    renderOrder(orderArray);
+  }
+  renderOrder(orderArray);
 }
 
 function renderOrder(orderArray) {
-    let foodList = '<p id="order-title">Your order</p>';
-    orderArray.forEach(item => {
-        foodList += `
+  let foodList = '<p id="order-title">Your order</p>';
+  orderArray.forEach((item) => {
+    foodList += `
         <div class="order-item">${item}</div>
-        `});
-    document.querySelector(".order").innerHTML = foodList + `
+        `;
+  });
+  document.querySelector(".order").innerHTML =
+    foodList +
+    `
     <hr>
     <div class="total">Total price:$${total} </div>
     <button class="btn" id="btn">Complete order</button>`;
-    console.log(orderArray)
-
+  console.log(orderArray);
 }
 
 function getMenuItems() {
-    let menuItems = '';
-    menuArray.forEach(function (item) {
-        menuItems += `
+  let menuItems = "";
+  menuArray.forEach(function (item) {
+    menuItems += `
         <div class="flex-container">
             <div class="emoji">
                 ${item.emoji}
@@ -54,21 +55,40 @@ function getMenuItems() {
              </div>
         </div>
         <hr>
-        `
-    })
-    return menuItems;
+        `;
+  });
+  return menuItems;
 }
 
 function render() {
-    document.querySelector(".menu").innerHTML = getMenuItems();
+  document.querySelector(".menu").innerHTML = getMenuItems();
 }
 
-render()
+render();
 
-const orderBtn = document.getElementById("btn")
+const orderClass = document.querySelector(".order");
 
-orderBtn.addEventListener("click", function () {
-    console.log("text")
-})
+orderClass.addEventListener("click", clickButton);
 
+function clickButton(e) {
+  if (e.target.id === "btn") {
+    modal.style.display = "inline";
+  }
+}
+
+const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const modalInner = document.getElementById("modal-inner");
+
+modalCloseBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+const payBtn = document.getElementById("pay-btn");
+
+payBtn.addEventListener("click", done);
+
+function done() {
+  modalInner.innerText = `Thank you for your order.`;
+}
 
